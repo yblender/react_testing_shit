a27bb6f72d8b47acfd6dbb5708edaee0cad5d4e9
yosef <yosef.bender@clade.com.au>
2023-01-30 13:52:19 +1100

    Cleaning up some shit

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,15 +1,10 @@
 import React, { useState } from 'react';
 import ScoreBoard from './game-src/ScoreBoard'
-import Ball from './game-src/Ball';
 import MathShit from './game-src/MathShit';
 
 
 function Game(){
-    const[isCaught, setIsCaught] = useState(false);
-    
-    function onClickBall(){
-        setIsCaught(true);
-    }
+   
     return(
         <div>
         <MathShit></MathShit>
